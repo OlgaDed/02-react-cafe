6e7b2d0b8270f1239f16288f79cb4b7dbcdf5383
OlgaDed <garmoneya@ukr.net>
2025-11-09 15:04:02 +0200

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
-
 import 'modern-normalize/modern-normalize.css';
-import './global.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
